58e6ce67d479ffa21a60cf9eb43d95ab77a068c4
Edwin <edwin.rubio@toshiba.com>
2026-05-27 13:01:26 -0500

    Added more to complete the project.

--- main.py
+++ main.py
@@ -6,11 +6,27 @@ def deal_card(deck):
     deck.append(random.choice(cards))
     return deck
 
-def calculate_score():
-    print()
+def calculate_score(cards):
+    total = sum(cards)
+    if total > 21 and 11 in cards:
+        total -= 10
+    return total
 
-def compare():
-    print()
+def compare(p_total, d_total):
+    if p_total == 21 and d_total == 21:
+        print("Is a tie!")
+    elif p_total == 21:
+        print("You win!")
+    elif d_total == 21:
+        print("You lose!") 
+    elif p_total > 21:
+        print("You lose!")
+    elif d_total > 21:
+        print("You win!")
+    elif p_total > d_total:
+        print("You win!")
+    else:
+        print("You lose!")
 
 start = 'y'
 
@@ -24,12 +40,19 @@ while start == 'y':
     dealer_cards = []
 
     deal_card(player_cards)
+    deal_card(player_cards)
     deal_card(dealer_cards)
 
-    print(f"Your cards: {player_cards}, current score: {sum(player_cards)}")
-    print(f"Dealer's first card: {dealer_cards[0]}")
+    draw = "y"
 
-    draw = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+    while draw == 'y':
+        print(f"Your cards: {player_cards}, current score: {calculate_score(player_cards)}")
+        print(f"Dealer's first card: {dealer_cards[0]}")
 
-    print(f"Your  final hand: {player_cards}, final score: {sum(player_cards)}")
-    print(f"Dealer's final hand: {dealer_cards}, final score: {sum(dealer_cards)}")
+        draw = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+        if draw == 'y':
+            deal_card(player_cards)
+
+    print(f"Your  final hand: {player_cards}, final score: {calculate_score(player_cards)}")
+    print(f"Dealer's final hand: {dealer_cards}, final score: {calculate_score(dealer_cards)}")
+    compare(calculate_score(player_cards), calculate_score(dealer_cards))
