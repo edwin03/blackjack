9605773c4b10605602543e83e6d84327f5bd03be
Edwin <edwin.rubio@ymail.com>
2026-05-28 19:59:32 +0000

    Finished the project.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from art import logo
-import random
+import random, os
 
 def deal_card(deck):
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
@@ -9,30 +9,39 @@ def deal_card(deck):
 def calculate_score(cards):
     total = sum(cards)
     if total > 21 and 11 in cards:
-        total -= 10
+        cards[cards.index(11)] = 1
     return total
 
 def compare(p_total, d_total):
-    if p_total == 21 and d_total == 21:
+    if p_total == d_total:
         print("Is a tie!")
     elif p_total == 21:
         print("You win!")
     elif d_total == 21:
-        print("You lose!") 
+        print("You lose!")
     elif p_total > 21:
-        print("You lose!")
+        print("You went over! You lose!")
     elif d_total > 21:
-        print("You win!")
-    elif p_total > d_total:
-        print("You win!")
+        print("Dealer went over! You win!")
     else:
-        print("You lose!")
+        if p_total > d_total:
+            print("You win!")
+        else:
+            print("You lose!")
+
+def clear_screen():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
 
 start = 'y'
 
 while start == 'y':
 
     start = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
+
+    clear_screen()
 
     print(logo)
 
@@ -50,9 +59,15 @@ while start == 'y':
         print(f"Dealer's first card: {dealer_cards[0]}")
 
         draw = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+    
         if draw == 'y':
             deal_card(player_cards)
+        if calculate_score(player_cards) > 21:
+            draw = 'n'
+        if calculate_score(dealer_cards) < 17:
+            deal_card(dealer_cards)
 
     print(f"Your  final hand: {player_cards}, final score: {calculate_score(player_cards)}")
     print(f"Dealer's final hand: {dealer_cards}, final score: {calculate_score(dealer_cards)}")
+   
     compare(calculate_score(player_cards), calculate_score(dealer_cards))
